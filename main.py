1707b6efdc23e1ef133c7e3758af620b01e7cac8
Dominic <dominic.nunez@gmail.com>
2018-10-19 14:41:15 -0500

    fixed single post link, display newest post at top of blog.html

--- main.py
+++ main.py
@@ -51,8 +51,7 @@ def favicon():
     return send_from_directory(
         os.path.join(app.root_path, "static"),
         "favicon.ico",
-        mimetype="image/vnd.microsoft.icon",
-    )
+        mimetype="image/vnd.microsoft.icon")
 
 
 @app.before_request
